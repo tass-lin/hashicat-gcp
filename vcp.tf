ccb7a787343004ca36b43ab1da981c02a7c812a5
tass <ienienien2001@gmail.com>
2021-06-16 03:00:02 +0000

    Added vpc module

---
+++ vcp.tf
@@ -0,0 +1,10 @@
+//--------------------------------------------------------------------
+// Modules
+module "network" {
+  source  = "app.terraform.io/tass/network/google"
+  version = "3.2.2"
+
+  network_name = "gaurav-network"
+  project_id = "p-hww6a4y9h4irwjc0hept513xu4ii"
+  subnets = "10.100.10.0/24"
+}
